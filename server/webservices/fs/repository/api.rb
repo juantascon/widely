dfa6773a2f28ccc795a9b5c8d54c9fb4d23cbf66
juandiego@localhost.localdomain <>
2007-04-16 18:55:22 -0500

    server/webservices/fs/*/api: now it works

--- server/webservices/fs/repository/api.rb
+++ server/webservices/fs/repository/api.rb
@@ -1,20 +1,24 @@
 module FS
 class Repository
 
-class API
-	extend WebService
+class API < Collection
+	include Singleton
+	include WebService
 	
-	def API.create(args)
+	def initialize
+		super()
+	end
+	
+	def create(args)
 		args_check(args, "manager", "dir")
 		obj = Repository.new(args["manager"].to_sym, args["dir"])
 		
-		return wso(obj)
+		return save_o(obj)
 	end
 	
 end
 
-Dispatcher.set_webservice("repos", API)
-
+Dispatcher.set_webservice("repos", API.instance)
 
 end
 end
--- server/webservices/fs/workingcopy/api.rb
+++ server/webservices/fs/workingcopy/api.rb
@@ -5,89 +5,93 @@
 module FS
 class WorkingCopy
 
-class API
+class API < Collection
+	include Singleton
+	include WebService
 	
-	extend WebService
+	def initialize
+		super()
+	end
 	
-	def API.create(args)
-		args_check(args, "wc_dir", "repository_wso_id")
+	def create(args)
+		args_check(args, "wc_dir", "repository_id")
 		
-		repository = find_wso(args["repository_wso_id"].to_i)
-		raise ArgumentError.new("repository[#{args["repository_wso_id"]}]: invalid") if ! repository
+		repository = Repository::API.instance.get_o(args["repository_id"].to_i)
+		raise ArgumentError.new("repository[#{args["repository_id"]}]: invalid") if ! repository
+		p repository
 		
 		obj = WorkingCopy.new(args["wc_dir"], repository)
 		
-		return wso(obj)
+		return save_o(obj)
 	end
 	
-	def API.checkout(args)
-		args_check(args, "wso_id")
-		obj = find_wso(args["wso_id"])
+	def checkout(args)
+		args_check(args, "wc_id")
+		obj = get_o(args["wc_id"].to_i)
 		
 		return obj.checkout(args["version"])
 	end
 
-	def API.commit(args)
-		args_check(args, "wso_id", "log")
-		obj = find_wso(args["wso_id"])
+	def commit(args)
+		args_check(args, "wc_id", "log")
+		obj = get_o(args["wc_id"])
 		
 		return obj.commit(args["log"])
 	end
 	
-	def API.versions(args)
-		args_check(args, "wso_id")
-		obj = find_wso(args["wso_id"])
+	def versions(args)
+		args_check(args, "wc_id")
+		obj = get_o(args["wc_id"])
 		
 		return obj.versions()
 	end
 	
-	def API.cat(args)
-		args_check(args, "wso_id", "path")
-		obj = find_wso(args["wso_id"])
+	def cat(args)
+		args_check(args, "wc_id", "path")
+		obj = get_o(args["wc_id"])
 		
 		return obj.cat(args["path"], args["version"])
 	end
 	
-	def API.ls(args)
-		args_check(args, "wso_id", "path")
-		obj = find_wso(args["wso_id"])
+	def ls(args)
+		args_check(args, "wc_id", "path")
+		obj = get_o(args["wc_id"])
 		
 		return obj.ls(args["path"], args["version"])
 	end
 	
-	def API.add(args)
-		args_check(args, "wso_id", "path")
-		obj = find_wso(args["wso_id"])
+	def add(args)
+		args_check(args, "wc_id", "path")
+		obj = get_o(args["wc_id"])
 		
 		as_dir = args["as_dir"] == "true"
 		
 		return obj.add(args["path"], as_dir)
 	end
 	
-	def API.delete(args)
-		args_check(args, "wso_id", "path")
-		obj = find_wso(args["wso_id"])
+	def delete(args)
+		args_check(args, "wc_id", "path")
+		obj = get_o(args["wc_id"])
 		
 		return obj.delete(args["path"])
 	end
 	
-	def API.move(path_from, path_to)
-		args_check(args, "wso_id", "path_from", "path_to")
-		obj = find_wso(args["wso_id"])
+	def move(path_from, path_to)
+		args_check(args, "wc_id", "path_from", "path_to")
+		obj = get_o(args["wc_id"])
 		
 		return obj.move(args["path_from"], args["path_to"])
 	end
 	
-	def API.write(args)
-		args_check(args, "wso_id", "path", "content")
-		obj = find_wso(args["wso_id"])
+	def write(args)
+		args_check(args, "wc_id", "path", "content")
+		obj = get_o(args["wc_id"])
 		
-		return obj.cat(args["path"], args["content"])
+		return obj.write(args["path"], args["content"])
 	end
 end
 
-Dispatcher.set_webservice("wc", API)
-
+Dispatcher.set_webservice("wc", API.instance)
 
 end
 end
