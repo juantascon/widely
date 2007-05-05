df123b2723723392bfeb998170d5fac2ac48d0cb
juandiego@localhost.localdomain <>
2007-05-05 15:48:48 -0500

    gui/class/ui/tree/{Tree, TreeView}: +multiple tree

--- gui/class/ui/tree/Tree.js
+++ gui/class/ui/tree/Tree.js
@@ -4,21 +4,19 @@ qx.Class.define("ui.tree.Tree",
 	
 	include: dao.Tree,
 	
-	construct: function () {
-		qx.ui.tree.Tree.call(this, "/");
+	construct: function (name) {
+		qx.ui.tree.Tree.call(this, name);
 		
 		this.set({height: "1*", width: "100%"});
 		this.setBackgroundColor(255);
 		this.setOverflow("auto");
 		this.setBorder(new qx.renderer.border.Border(1, "solid", "#91A5BD"));
-		this.load();
 	},
 	
 	members:
 	{
 		load_from_hash: function(data) {
 			this.destroyContent();
-			
 			for (var i in data){
 				if (data[i]["type"] == "dir"){
 					this.addToFolder(ui.tree.Dir.new_from_hash(data[i]));
@@ -27,6 +25,15 @@ qx.Class.define("ui.tree.Tree",
 					this.addToFolder(ui.tree.File.new_from_hash(data[i]));
 				}
 			}
+		}
+	},
+	
+	statics:
+	{
+		new_from_hash: function(name, data) {
+			var tree = new ui.tree.Tree(name);
+			tree.load_from_hash(data);
+			return tree;
 		}
 	}
 });
--- gui/class/ui/tree/TreeView.js
+++ gui/class/ui/tree/TreeView.js
@@ -8,15 +8,27 @@ qx.Class.define("ui.tree.TreeView",
 		qx.ui.layout.VerticalBoxLayout.call(this);
 		this.set({height: "100%", width: "100%"});
 		
-		this.setTree(new ui.tree.Tree());
 		this.setToolbar(new ui.tree.ToolBar());
 		this.add(this.getToolbar());
-		this.add(this.getTree());
+		
+		this.setTree(new ui.tree.Tree("/"));
 	},
 	
 	properties:
 	{
-		tree: { check: "qx.ui.tree.Tree" },
+		tree: { check: "qx.ui.tree.Tree", apply: "change_tree" },
 		toolbar: { check: "ui.tree.ToolBar" }
+	},
+	
+	members:
+	{
+		change_tree: function(newValue, oldValue) {
+			if (oldValue) {
+				oldValue.destroyContent();
+				oldValue.dispose();
+				this.remove(oldValue);
+			}
+			this.add(newValue);
+		}
 	}
 });
