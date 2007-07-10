80c2ce60487f543f24940209b4aea893ceb5333e
juandiego@juandiego <>
2007-07-10 01:01:55 -0500

    gui/class/lib/form: 	+Helper: generic combobox,dialog,grid creation 	NewWC: Helper adapted

---
+++ gui/class/lib/form/Helper.js
@@ -0,0 +1,74 @@
+qx.Mixin.define("lib.form.Helper",
+{
+	members:
+	{
+		create_dialog: function(pmodal, message) {
+			var d = new lib.ui.WDialog(pmodal, message, this);
+			d.addEventListener("ok", function(e) {
+				this.debug(this.getName_i().getComputedValue());
+				this.debug(this.getManager_i().getSelectedRow()[1]);
+				this.debug(this.getRepo_i().getSelectedRow()[1]);
+			}, this);
+			
+			return d;
+		},
+		
+		create_grid: function(labels, inputs, h, w) {
+			var grid = new qx.ui.layout.GridLayout();
+			with(grid) {
+				set({left: 0, top: 0, height: h, width: w});
+				
+				setColumnCount(2);
+				setColumnWidth(0, "40%");
+				setColumnWidth(1, "60%");
+				
+				setRowCount(labels.length);
+				for (var i in labels) {
+					setRowHeight(i, ""+parseInt(100/labels.length)+"%");
+					add(new qx.ui.basic.Label(labels[i]), 0, i);
+				}
+				
+				for (var i in inputs) {
+					add(inputs[i], 1, i);
+				}
+			}
+			
+			return grid;
+		},
+		
+		create_combobox: function(headers, wrq) {
+			var c = new qx.ui.form.ComboBoxEx();
+			
+			with(c) {
+				set({left: 0, top: 0, height: "auto", width: "auto"});
+				var arr = qx.lang.Array.insertAt(qx.lang.Array.copy(headers), "ID", 0);
+				for (var i in arr) {
+					arr[i] = qx.lang.String.toFirstUp(arr[i]);
+				}
+				setColumnHeaders(arr);
+			}
+			
+			wrq.addEventListener("ok", function(e) {
+				var list = e.getData();
+				var selection = [];
+				
+				for(var i in list) {
+					var item = [];
+					item.push(i);
+					
+					for (var h in headers) {
+						item.push(list[i][headers[h]]);
+					}
+					
+					selection.push(item);
+				}
+				
+				c.setSelection(selection);
+				c.setSelectedIndex(0);
+				
+			}, this);
+			
+			return c;
+		}
+	}
+});
--- gui/class/lib/form/NewWC.js
+++ gui/class/lib/form/NewWC.js
@@ -1,102 +1,34 @@
-// data: name, manager, repo_id
-
 qx.Class.define("lib.form.NewWC",
 {
 	extend: qx.ui.groupbox.GroupBox,
 	
-	include: [ lib.dao.WC, lib.dao.Repo ],
+	include: [ lib.dao.WC, lib.dao.Repo, lib.form.Helper ],
 	
 	properties:
 	{
 		grid: { check: "qx.ui.layout.GridLayout" },
 		name_i: { check: "qx.ui.form.TextField" },
-		manager_i: { check: "qx.ui.form.ComboBox" },
+		manager_i: { check: "qx.ui.form.ComboBoxEx" },
 		repo_i: { check: "qx.ui.form.ComboBoxEx" }
 	},
 	
 	construct: function () {
 		this.base(arguments, "Working Copy Info");
-		this.setGrid(new qx.ui.layout.GridLayout());
 		this.set({left: 0, top: 0, height: "auto", width: "auto"});
 		
 		this.setName_i(new qx.ui.form.TextField(""));
-		this.getName_i().set({left: 0, top: 0, height: "auto", width: "auto"});
+		this.setRepo_i(this.create_combobox( ["name", "manager"], this.repo_list() ));
+		this.setManager_i(this.create_combobox( ["name", "description"], this.wc_manager_list() ));
 		
-		this.setManager_i(new qx.ui.form.ComboBox());
-		//this.getManager_i().set({left: 0, top: 0, height: "auto", width: "auto"});
-		
-		var rq_managers = this.wc_manager_list();
-		rq_managers.addEventListener("ok", function(e) {
-			var m = this.getManager_i();
-			var list = e.getData();
-			
-			for(var i in list) {
-				m.add(new qx.ui.form.ListItem(list[i], null, list[i]));
-			}
-			
-			m.setSelected(m.getList().getFirstChild());
-		}, this);
-		
-		
-		this.setRepo_i(new qx.ui.form.ComboBoxEx());
-		with(this.getRepo_i()) {
-			//set({left: 0, top: 0, height: "auto", width: "auto"});
-			setColumnHeaders(["ID", "Name", "Manager"]);
-			//setShowOnTextField("Name");
-		}
-		
-		var rq_repos = this.repo_list();
-		rq_repos.addEventListener("ok", function(e) {
-			var r = this.getRepo_i();
-			var list = e.getData();
-			var selection = [];
-			
-			for(var i in list) {
-				selection.push([ i, list[i]["name"], list[i]["manager"] ]);
-			}
-			
-			r.setSelection(selection);
-			r.setSelectedIndex(0);
-			
-		}, this);
-		
-		
-		
-		with(this.getGrid()) {
-			set({left: 0, top: 0, height: 80, width: 200});
-			setColumnCount(2);
-			setRowCount(3);
-			
-			setColumnWidth(0, "40%");
-			setColumnWidth(1, "60%");
-			
-			setRowHeight(0, "33%");
-			setRowHeight(1, "33%");
-			setRowHeight(2, "33%");
-			
-			add(new qx.ui.basic.Label("Name") , 0, 0);
-			add(new qx.ui.basic.Label("Manager"), 0, 1);
-			add(new qx.ui.basic.Label("Repository"), 0, 2);
-			
-			add(this.getName_i(), 1, 0);
-			add(this.getManager_i(), 1, 1);
-			add(this.getRepo_i(), 1, 2);
-		}
+		this.setGrid(this.create_grid(
+			[ "Name", "Manager", "Repository" ],
+			[ this.getName_i(), this.getManager_i(), this.getRepo_i() ],
+			80, 200
+		));
 		
 		this.add(this.getGrid());
-	},
-	
-	members:
-	{
-		show_dialog: function(pmodal) {
-			var dialog = new lib.ui.WDialog(pmodal, "New Working Copy", this);
-			dialog.addEventListener("ok", function(e) {
-				this.debug(this.getName_i().getComputedValue());
-				this.debug(this.getManager_i().getSelected().getValue());
-				this.debug(this.getRepo_i().getSelectedRow()[1]);
-			}, this);
-		}
 	}
+	
 });
 
 
