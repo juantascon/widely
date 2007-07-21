7aba0a01e0f09ec865e5311c667d6a0282f51aad
juandiego@localhost.localdomain <>
2007-07-21 14:09:20 -0500

    tests/webservices/login: +user/create

--- tests/webservices/login.sh
+++ tests/webservices/login.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
-id=$(obj_ws auth/login "user_id=test&password=test") ; echo $id
 
 admin_id=$(obj_ws auth/login_admin "password=admin") ; echo $admin_id
+raw_ws user/create "session_id=$admin_id&user_id=test&password=test"
+
+id=$(obj_ws auth/login "user_id=test&password=test") ; echo $id
 
 id=$(obj_ws auth/user_session "session_id=$admin_id&user_id=test") ; echo $id
