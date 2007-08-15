add1bd5e88e195530164ff80c06f39b8982bc6e2
juandiego@juandiego <>
2007-08-15 03:41:39 -0500

    doc/diagrams/: 	*.dia,ui/*: page size update 	build.sh: update

--- doc/diagrams/build.sh
+++ doc/diagrams/build.sh
@@ -13,13 +13,18 @@ mkdir -p $OUTPUT
 
 for d in $DDIR/*.dia
 do
-	dia -e "$OUTPUT/$(basename $d).png" "$d"
-	dia -e "$OUTPUT/$(basename $d).eps" "$d"
+	#dia -t png -e "$OUTPUT/$(basename $d .dia).png" "$d"
+	dia -t eps-pango -e "$OUTPUT/$(basename $d .dia).eps" "$d"
 done
 
 for d in $DDIR/ui/*.svg
 do
-	inkscape -b white -e "$OUTPUT/ui-$(basename $d).png"  "$d"
-	inkscape -b white -E "$OUTPUT/ui-$(basename $d).eps"  "$d"
+	#inkscape -b white -e "$OUTPUT/ui-$(basename $d .svg).png"  "$d"
+	inkscape -b white -E "$OUTPUT/ui-$(basename $d .svg).eps"  "$d"
+done
+
+for eps in "$OUTPUT"/*.eps
+do
+	epstopdf $eps
 done
 
